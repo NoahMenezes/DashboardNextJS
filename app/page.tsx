9b678757c30abbdda6e056fe34688142185ef81c
Noah Menezes <NoahMenezes@users.noreply.github.com>
2026-01-06 21:34:36 +0530

    first push

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "@/components/hero-section";
+
+export default function Home() {
+  return (
+    <div className="bg-background min-h-screen">
+      <HeroSection />
+    </div>
+  );
+}
